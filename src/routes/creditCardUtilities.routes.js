92281fd98fc9de8647e1f07032f39834484b2436
user1 <mh.torneria@gmail.com>
2024-05-13 12:54:21 -0400

    modificación de endpoint para obtener datos de Bin

--- src/routes/creditCardUtilities.routes.js
+++ src/routes/creditCardUtilities.routes.js
@@ -5,7 +5,7 @@ const router = express.Router()
 const raiz = '/card'
 
 router.get(`${raiz}/:number`, getCardData)
-router.get(`${raiz}/:bin`, getBinData)
+router.get(`${raiz}/bin/:bin`, getBinData)
 router.get(`/getBanks/:aimCountry`, getBanks)
 router.get(`/getNetworks/:aimBank`, getNewtwork)
 router.get(`/getcountries`, getCountries)
